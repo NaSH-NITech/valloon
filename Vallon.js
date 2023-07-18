2adb6023a1f03abcd754eb0379ca9229f2115660
kouya0205 <kouya0205game@gmail.com>
2023-07-18 18:23:06 +0900

    js

--- Vallon.js
+++ Vallon.js
@@ -1,8 +1,8 @@
-// let e = document.getElementsByClassName("come-in");
-// e[0].addEventListener("click",function(){
-//     addElement()
-// });
-// function addElement() {
-//     let Text = document.getElementById("txt");
+let e = document.getElementsByClassName("come-in");
+e[0].addEventListener("click",function(){
+    addElement()
+});
+function addElement() {
+    let Text = document.getElementById("txt");
 
-// }
+}
