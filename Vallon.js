f75c1d58bf5459704b1e356c95656011575ad5fb
kouya0205 <kouya0205game@gmail.com>
2023-07-21 00:50:52 +0900

    要素の下に取得した要素を追加

--- Vallon.js
+++ Vallon.js
@@ -22,9 +22,12 @@ function clickBtn() {
   let Text = document.getElementById("name");
   let text = document.createTextNode(Text.value);
   let li = document.createElement("li");
+  const uniqueID = "element_" + Date.now();
+  li.setAttribute("id", uniqueID)
   let list = document.getElementById('come');
   li.appendChild(text);
   list.appendChild(li);
+  const styleElement = document.createElement("style");
+  styleElement.textContent = `#${uniqueID} { list-style: none; border-bottom: 1px solid; }`;
+  document.head.appendChild(styleElement);
 }
-
-
