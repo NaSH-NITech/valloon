71e00281427e57b592bd742003273fbf5538ee73
kouya0205 <kouya0205game@gmail.com>
2023-07-20 21:29:22 +0900

    名前の要素の取り出しと表示

--- Vallon.js
+++ Vallon.js
@@ -1,20 +1,30 @@
 
-document.getElementsByClassName('come-in').addEventListener('click',function(){
-   var element = document.getElementById('flame');
-   if (element.style.display === 'none') {
-      element.style.display = 'block';  
-    } else {
-      element.style.display = 'none';  
-}
+// document.getElementsByClassName('come-in').addEventListener('click',function(){
+//    var element = document.getElementById('flame');
+//    if (element.style.display === 'none') {
+//       element.style.display = 'block';  
+//     } else {
+//       element.style.display = 'none';  
+// }
 
-})
+// })
 
 
+// function clickBtn() {
+//   const t1 = document.getElementById("name").value;
+//   document.getElementById("h1").textContent = t1;
+// }
+
+// function Textchange() {
+//   document.getElementById("h1").innerHTML = "name";
+// }
 function clickBtn() {
-  const t1 = document.getElementById("name").value;
-  document.getElementById("h1").textContent = t1;
+  let Text = document.getElementById("name");
+  let text = document.createTextNode(Text.value);
+  let li = document.createElement("li");
+  let list = document.getElementById('come');
+  li.appendChild(text);
+  list.appendChild(li);
 }
-function Textchange() {
-  document.getElementById("h1").innerHTML = "<span>name</span>";
 
-}
+
